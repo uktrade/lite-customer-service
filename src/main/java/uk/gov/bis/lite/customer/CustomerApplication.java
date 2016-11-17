71ed1f4e6071b2499f9eaa1f8100a9c689c80a48
Dan Haynes <djm.haynes@gmail.com>
2016-11-17 15:44:43 +0000

    jersey-correlation-id integration

--- src/main/java/uk/gov/bis/lite/customer/CustomerApplication.java
+++ src/main/java/uk/gov/bis/lite/customer/CustomerApplication.java
@@ -6,6 +6,7 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import ru.vyarus.dropwizard.guice.GuiceBundle;
 import ru.vyarus.dropwizard.guice.module.installer.feature.jersey.ResourceInstaller;
+import uk.gov.bis.lite.common.jersey.filter.ContainerCorrelationIdFilter;
 import uk.gov.bis.lite.common.spire.client.exception.SpireClientException;
 import uk.gov.bis.lite.customer.config.CustomerApplicationConfiguration;
 import uk.gov.bis.lite.customer.config.guice.GuiceModule;
@@ -24,6 +25,7 @@ public class CustomerApplication extends Application<CustomerApplicationConfigur
   @Override
   public void run(CustomerApplicationConfiguration configuration, Environment environment) {
     environment.jersey().register(SpireClientException.ServiceExceptionMapper.class);
+    environment.jersey().register(ContainerCorrelationIdFilter.class);
   }
 
   @Override
