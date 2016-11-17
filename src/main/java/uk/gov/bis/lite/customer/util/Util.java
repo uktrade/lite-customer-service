379973ae716a7a20ae939bfd0e1d4545b20cfc2a
Dan Haynes <djm.haynes@gmail.com>
2016-11-17 13:28:52 +0000

    Updated friendly address to ignore both null and empty

--- src/main/java/uk/gov/bis/lite/customer/util/Util.java
+++ src/main/java/uk/gov/bis/lite/customer/util/Util.java
@@ -4,9 +4,15 @@ package uk.gov.bis.lite.customer.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.bis.lite.customer.api.param.AddressParam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Util {
 
@@ -18,10 +24,13 @@ public class Util {
     return arg == null ? "false" : arg ? "true" : "false";
   }
 
+  /**
+   * New line delimited address String omitting any nulls or empty
+   */
   public static String getFriendlyAddress(AddressParam param) {
-    return Joiner.on("\n").skipNulls()
-        .join(param.getLine1(), param.getLine2(), param.getTown(),
-            param.getPostcode(), param.getCounty(), param.getCountry());
+    List<String> addressLines = new ArrayList<>(Arrays.asList(param.getLine1(), param.getLine2(), param.getTown(),
+        param.getPostcode(), param.getCounty(), param.getCountry()));
+    return Joiner.on("\n").join(Iterables.filter(addressLines, StringUtils::isNotBlank));
   }
 
   public static String getAddressParamJson(AddressParam param) {
