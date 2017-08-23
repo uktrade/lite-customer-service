13efa786a6b10536c0e03f241c8894f799cda11e
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-08-23 14:50:03 +0100

    Updates spire-client to v1.5

--- src/main/java/uk/gov/bis/lite/customer/exception/SpireForbiddenException.java
+++ src/main/java/uk/gov/bis/lite/customer/exception/SpireForbiddenException.java
@@ -1,11 +1,8 @@
 package uk.gov.bis.lite.customer.exception;
 
-import io.dropwizard.jersey.errors.ErrorMessage;
+import javax.ws.rs.WebApplicationException;
 
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-
-public class SpireForbiddenException extends RuntimeException {
+public class SpireForbiddenException extends WebApplicationException {
 
   /**
    * SpireForbiddenException
@@ -13,17 +10,6 @@ public class SpireForbiddenException extends RuntimeException {
    * @param info information on exception
    */
   public SpireForbiddenException(String info) {
-    super("Spire Forbidden Exception: " + info);
-  }
-
-  /**
-   * Provided for Dropwizard/Jersey integration
-   */
-  public static class ServiceExceptionMapper implements ExceptionMapper<SpireForbiddenException> {
-
-    @Override
-    public Response toResponse(SpireForbiddenException exception) {
-      return Response.status(403).entity(new ErrorMessage(403, exception.getMessage())).build();
-    }
+    super("Spire Forbidden Exception: " + info, 403);
   }
 }
