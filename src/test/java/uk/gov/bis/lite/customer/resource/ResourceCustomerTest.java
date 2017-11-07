bf2ec2af33eeee10622c1a80800c2c0abcb16a43
Laurence Cartwright <laurence.cartwright@fivium.co.uk>
2017-11-07 18:08:03 +0000

    Jwt auth added to CustomerResource

--- src/test/java/uk/gov/bis/lite/customer/resource/ResourceCustomerTest.java
+++ src/test/java/uk/gov/bis/lite/customer/resource/ResourceCustomerTest.java
@@ -2,6 +2,7 @@ package uk.gov.bis.lite.customer.resource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static uk.gov.bis.lite.customer.JwtUtil.generateToken;
 
 import org.junit.Test;
 import uk.gov.bis.lite.customer.api.view.CustomerView;
@@ -27,16 +28,34 @@ public class ResourceCustomerTest extends SpireResourceTest {
 
   @Test
   public void customers() {
-    Response response = request("/customers/EXISTING_CUSTOMER").get();
+    Response response = request("/customers/EXISTING_CUSTOMER")
+        .header("Authorization", "Bearer " + generateToken(JWT_SHARED_SECRET, "123456"))
+        .get();
     assertThat(status(response)).isEqualTo(OK);
-    //assertThat(getCustomersSize(response)).isEqualTo(MOCK_CUSTOMERS_NUMBER);
+    assertThat(getResponseCustomerView(response).getCustomerId()).isEqualTo(MOCK_CUSTOMER_ID);
+
+    // Without valid auth header
+    response = request("/customers/EXISTING_CUSTOMER").get();
+    assertThat(status(response)).isEqualTo(UNAUTHORIZED);
   }
 
   @Test
   public void userCustomers() {
-    Response response = request("/user-customers/user/1").get();
+    Response response = request("/user-customers/user/1")
+        .header("Authorization", "Bearer " + generateToken(JWT_SHARED_SECRET, "1"))
+        .get();
     assertThat(status(response)).isEqualTo(OK);
     assertThat(getCustomerViewsSize(response)).isEqualTo(MOCK_CUSTOMERS_NUMBER);
+
+    // With mismatched user id (1) to jwt subject (123456)
+    response = request("/user-customers/user/1")
+        .header("Authorization", "Bearer " + generateToken(JWT_SHARED_SECRET, "123456"))
+        .get();
+    assertThat(status(response)).isEqualTo(UNAUTHORIZED);
+
+    // Without valid auth header
+    response = request("/user-customers/user/1").get();
+    assertThat(status(response)).isEqualTo(UNAUTHORIZED);
   }
 
   @Test
@@ -45,31 +64,49 @@ public class ResourceCustomerTest extends SpireResourceTest {
     String url = "/search-customers/org-info";
 
     // Missing required param
-    Response response = request(url).get();
+    Response response = request(url)
+        .header("Authorization", "Bearer " + generateToken(JWT_SHARED_SECRET, "123456"))
+        .get();
     assertEquals(400, response.getStatus());
 
     // With postcode param
-    response = target(url).queryParam("postcode", "postcode").request().get();
+    response = target(url).queryParam("postcode", "postcode").request()
+        .header("Authorization", "Bearer " + generateToken(JWT_SHARED_SECRET, "123456"))
+        .get();
     assertThat(status(response)).isEqualTo(OK);
     assertThat(getCustomerViewsSize(response)).isEqualTo(MOCK_CUSTOMERS_NUMBER);
 
     // With eori param
-    response = target(url).queryParam("eori", "eori").request().get();
+    response = target(url).queryParam("eori", "eori").request()
+        .header("Authorization", "Bearer " + generateToken(JWT_SHARED_SECRET, "123456"))
+        .get();
     assertEquals(400, response.getStatus());
 
     // With postcode and eori param
     response = target(url).queryParam("postcode", "postcode")
-        .queryParam("eori", "eori").request().get();
+        .queryParam("eori", "eori").request()
+        .header("Authorization", "Bearer " + generateToken(JWT_SHARED_SECRET, "123456"))
+        .get();
     assertThat(status(response)).isEqualTo(OK);
     assertThat(getCustomerViewsSize(response)).isEqualTo(MOCK_CUSTOMERS_NUMBER);
+
+    // Without valid auth header
+    response = request(url).get();
+    assertThat(status(response)).isEqualTo(UNAUTHORIZED);
   }
 
   @Test
   public void searchCustomersByCompanyNumber() {
-    Response response = request("/search-customers/registered-number/1").get();
+    Response response = request("/search-customers/registered-number/1")
+        .header("Authorization", "Bearer " + generateToken(JWT_SHARED_SECRET, "123456"))
+        .get();
     assertThat(status(response)).isEqualTo(OK);
     CustomerView customer = getCustomerResponse(response);
     assertThat(customer.getCustomerId()).isEqualTo(MOCK_CUSTOMERS_SAR_REF_TAG + "1");
+
+    // Without valid auth header
+    response = request("/search-customers/registered-number/1").get();
+    assertThat(status(response)).isEqualTo(UNAUTHORIZED);
   }
 
 }
